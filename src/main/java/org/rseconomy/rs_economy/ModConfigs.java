a656c55059e829f7c02a39e67d3769ce4b5b7069
Rosti <mika.rostalski@outlook.com>
2025-09-23 18:36:41 +0200

    Add default configuration

--- src/main/java/org/rseconomy/rs_economy/ModConfigs.java
+++ src/main/java/org/rseconomy/rs_economy/ModConfigs.java
@@ -7,21 +7,31 @@ import java.nio.file.Path;
 
 public class ModConfigs {
     public static final ModConfigSpec COMMON_CONFIG;
-    public static final ModConfigSpec.BooleanValue FEATURE_X;
-    public static final ModConfigSpec.IntValue MAX_Y;
+    public static final ModConfigSpec.ConfigValue<String> CURRENCY;
+    public static final ModConfigSpec.ConfigValue<String> LOCALE;
+    public static final ModConfigSpec.ConfigValue<Integer> DAILY_REWARD_MIN;
+    public static final ModConfigSpec.ConfigValue<Integer> DAILY_REWARD_MAX;
 
     static {
         ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
 
-        builder.comment("Allgemeine Einstellungen").push("general");
+        builder.comment("RS-ECONOMY GENERAL CONFIG").push("general");
 
-        FEATURE_X = builder
-                .comment("Soll Feature X aktiviert sein?")
-                .define("featureX", true);
+        CURRENCY = builder
+                .comment("Currency name used in the economy system")
+                .define("currency", "Coins", s -> s instanceof String && !((String) s).isEmpty());
 
-        MAX_Y = builder
-                .comment("Maximale Anzahl von Y")
-                .defineInRange("maxY", 10, 1, 100);
+        LOCALE = builder
+                .comment("Locale for the mod (e.g., en_US, de_DE)")
+                .define("locale", "en_US", s -> s instanceof String && ((String) s).matches("[a-z]{2}_[A-Z]{2}"));
+
+        DAILY_REWARD_MIN = builder
+                .comment("Minimum daily reward amount")
+                .define("daily.reward.min", 100, i -> i instanceof Integer);
+
+        DAILY_REWARD_MAX = builder
+                .comment("Maximum daily reward amount")
+                .define("daily.reward.max", 500, i -> i instanceof Integer);
 
         builder.pop();
 
